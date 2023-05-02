6138400a3243849beb253d8536b730c5648e97b4
João <joaovictorpfr@gmail.com>
2023-05-02 15:16:25 -0300

    start server

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 import http from 'http';
-
 const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('NodeJS server iniciado');
+})
+
+server.listen(port, () => {
+    console.log(`Start port ${port}`);
+})
