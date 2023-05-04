9fe35aac4ccd0aa1be132118999a44cc85f34ebb
João <joaovictorpfr@gmail.com>
2023-05-04 18:55:31 -0300

    db on and once for db connect

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ import { db } from "./config/dbConnect.js"
 import { books } from "./api/books.js";
 import { users } from "./api/users.js";
 
-db.on()
+db.on("error", console.log.bind(console, "Erro de conexão!"));
+db.once("open", () => {
+    console.log("Conexão instaurada!")
+});
+
 export const app = express();
 app.use(express.json());
 
