a63e6a484cf3c300c1160e1551eacd391e67a292
João <joaovictorpfr@gmail.com>
2023-05-04 16:11:01 -0300

    add CRUD API

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const http = require ('http');
-const port = 3000;
+import { app } from './src/app.js';
 
+const port = process.env.PORT || 3000;
+
+/*
 const rotas = {
     '/': 'Curso de NodeJS',
-    '/livros': 'Página de livros',
-    '/autores': 'Página de autores'
+    '/books': 'Página de livros',
+    '/authors': 'Página de autores'
 }
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(rotas[req.url]);
 })
+*/
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Start port ${port}`);
 })
---
+++ src/app.js
@@ -0,0 +1,54 @@
+import express from "express";
+import { books } from "./books.js";
+export const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Curso de Node");
+});
+
+app.get("/books", (req, res) => {
+  res.status(200).json(books);
+});
+
+app.get("/books/:id", (req, res) => {
+  let index = getBooks(req.params.id);
+  res.json(books[index]);
+});
+
+app.post("/books", (req, res) => {
+  books.push(req.body);
+  res.status(201).send("book foi cadastrado com sucesso");
+});
+
+app.post("/books/:id", (req, res) => {
+  books[req.params.id].push(req.body);
+  res.status(201).send("book foi cadastrado com sucesso");
+});
+
+
+app.put("/books/:id", (req, res) => {
+  let index = getBooks(req.params.id);
+  books[index].title = req.body.title;
+  res.json(books);
+});
+
+app.delete("/books/:id", (req, res) => {
+  let { id } = req.params;
+  let index = getBooks(id);
+  books.splice(index, 1);
+  res.send(`book ${id} removido com sucesso`);
+});
+
+function getBooks(id) {
+  return books.findIndex((book) => book.id == id);
+}
+
+app.delete('books/:id', (req, res) => {
+    let {id} = req.params;
+    let index = getBooks(id);
+    books.splice(index, 1);
+    res.send(`book ${id} removido!`)
+    res.json(books);
+})
